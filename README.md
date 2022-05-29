658df55ebc2923828dfa0303add3fc144b393316
Adrian Lee <alee.lcagency@gmail.com>
2022-05-30 00:06:34 +0800

    feat: quotation endpoints (#4)
    Co-authored-by: adrian.lee <adrian.lee@lalamove.com>

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 This is an API in [Deno Oak](https://deno.land/x/oak@v10.5.1) framework based on
 the self code challenge described in this
 [link](https://github.com/LCYAD/self-code-challenge)
+
+## Prerequisite
+
+- Things to install
+  - [Deno](https://deno.land/manual/getting_started/installation)
+  - docker-compose
+
+## Setup Services
+
+```bash
+# To boot up the services
+docker-compose -f docker-compose-ldev.yml up -d
+
+# To close the services
+docker-compose -f docker-compose-ldev.yml down
+```
--- handlers/base.ts
+++ handlers/base.ts
@@ -1,8 +1,9 @@
 import type { Context } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import type { ResponseBody } from "https://deno.land/x/oak@v10.6.0/response.ts";
 import { parse } from "https://deno.land/std@0.140.0/encoding/yaml.ts";
 
 export const swaggerDocHanlder = async (ctx: Context) => {
   const swaggerStr = await Deno.readTextFile("./openapi.yml");
-  const swaggerJSON = parse(swaggerStr);
-  ctx.response.body = swaggerJSON as any;
+  const swaggerJSON = parse(swaggerStr) as ResponseBody;
+  ctx.response.body = swaggerJSON;
 };
