63d464f780403a5dc63ce99af413e4e736f722af
Adrian Lee <alee.lcagency@gmail.com>
2022-05-29 23:30:00 +0800

    feat: swagger setup (#3)
    Co-authored-by: adrian.lee <adrian.lee@lalamove.com>

--- handlers/base.ts
+++ handlers/base.ts
@@ -1,5 +1,8 @@
 import type { Context } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import { parse } from "https://deno.land/std@0.140.0/encoding/yaml.ts";
 
-export const swaggerDocHanlder = (ctx: Context) => {
-  ctx.response.body = { api: "/" };
+export const swaggerDocHanlder = async (ctx: Context) => {
+  const swaggerStr = await Deno.readTextFile("./openapi.yml");
+  const swaggerJSON = parse(swaggerStr);
+  ctx.response.body = swaggerJSON as any;
 };
