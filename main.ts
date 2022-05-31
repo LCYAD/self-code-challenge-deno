86f44190e62f3ac62618f952095ed5dec189b2f0
Adrian Lee <alee.lcagency@gmail.com>
2022-05-31 11:25:08 +0800

    feat: .env setup (#7)
    Co-authored-by: adrian.lee <adrian.lee@lalamove.com>

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import { errorHandlerMiddleware } from "./middlewares/errorHandler.ts";
 import { baseRouter } from "./routers/base.ts";
 import { swaggerRouter } from "./routers/swagger.ts";
@@ -12,4 +13,6 @@ app.use(swaggerRouter.routes());
 app.use(quotationRouter.routes());
 app.use(baseRouter.routes());
 
-await app.listen({ port: 4000 });
+await app.listen({
+  port: parseInt(Deno.env.get("PORT") as string ?? 3000, 10),
+});
